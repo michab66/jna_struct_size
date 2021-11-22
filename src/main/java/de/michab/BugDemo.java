662a9a71a3f244b3130871b394b8a76dafe8dc71
Michael Binz <michael.binz@daimler.com>
2021-11-22 14:17:04 +0100

    Project init.

---
+++ src/main/java/de/michab/BugDemo.java
@@ -0,0 +1,69 @@
+package de.michab;
+
+import com.sun.jna.Structure;
+import com.sun.jna.Structure.FieldOrder;
+
+class BugDemo
+{
+    @FieldOrder(value = { "wSize", "buffer" })
+    public static class DYNAMIC_STRUCT extends Structure
+    {
+        public DYNAMIC_STRUCT( int bufferSize ) {
+            super( ALIGN_NONE );
+            if ( bufferSize < 1 )
+                throw new IllegalArgumentException( "size < 1" );
+            wSize = bufferSize;
+            buffer = new byte[ bufferSize ];
+        }
+        public DYNAMIC_STRUCT() {
+            this( 1 );
+        }
+
+        public int wSize;
+        public byte[] buffer;
+    }
+
+    @FieldOrder(value = { "dynamic", "terminal" })
+    public static class CONTAINER extends Structure
+    {
+        public CONTAINER( int cfgDescriptorSize ) {
+            dynamic = new DYNAMIC_STRUCT( cfgDescriptorSize );
+            setAlignType( ALIGN_NONE );
+        }
+        public CONTAINER() {
+            this( 1 );
+        }
+
+        @Override
+        public int size()
+        {
+            return super.size() -1;
+        }
+
+        public DYNAMIC_STRUCT dynamic;
+        public byte[] terminal = new byte[1];
+    }
+
+    private static void expect( int expected, int actual ) {
+        if ( expected != actual )
+            System.out.format( "Expected %d, got %d%n", expected, actual );
+    }
+
+    public static void main( String[] args )
+    {
+        {
+            DYNAMIC_STRUCT cd = new DYNAMIC_STRUCT( 1 );
+            expect( 4+1, cd.size() );
+            cd = new DYNAMIC_STRUCT( 10 );
+            expect( 4+10, cd.size() );
+        }
+
+        {
+            CONTAINER combined = new CONTAINER( 1 );
+            expect( 4+1, combined.size() );
+            // This fails: 'Expected 14, got 5'
+            combined = new CONTAINER( 10 );
+            expect( 4+10, combined.size() );
+        }
+    }
+}
